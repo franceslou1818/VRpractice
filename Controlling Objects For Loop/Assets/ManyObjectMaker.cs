d480eaac3d6f81d3f02ad4f2d41065d538bf593d
Frances Ramirez <franceslou_1818@yahoo.com>
2018-09-19 13:34:36 +0100

    finished lesson 2 of Udacity VR software dev

---
+++ Controlling Objects For Loop/Assets/ManyObjectMaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManyObjectMaker : MonoBehaviour {
+
+	public GameObject objectToCreate;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i<1000; i++) {
+			Object.Instantiate(objectToCreate, new Vector3(i,4,6), Quaternion.identity);	
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VR-Software-Development_Controlling-Objects-Using-Code-2.0.0/Controlling Objects Using Code/Assets/ManyObjectMaker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManyObjectMaker : MonoBehaviour {
+
+	public GameObject objectToCreate;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i<50; i++) {
+			//Object.Instantiate(objectToCreate, new Vector3(i,4,6), Quaternion.identity);
+
+
+			//the same as above but storing it in a var to let us change it
+			GameObject newSeagull = (GameObject)Object.Instantiate(objectToCreate, new Vector3(i,0,0), Quaternion.identity);
+			// seagull prefab in assets has hierarchy and if we expand that hierarchy, that would be the renderer
+			// this will find the first renderer on the new seagull
+			// this case first renderer is named seagull under seagullmodel
+			Renderer objectRenderer = newSeagull.GetComponentInChildren<Renderer>();
+			// it will find that material (one of the components) and change the color
+				// random = random shades of white
+			//objectRenderer.material.color = Color.white * Random.value;
+			objectRenderer.material.color = Random.ColorHSV(); // try this instead
+
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
