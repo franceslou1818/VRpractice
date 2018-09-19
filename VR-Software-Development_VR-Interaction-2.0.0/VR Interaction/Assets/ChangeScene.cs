97bf53092925ae10e6ce9d3d889dc4061fee82aa
Frances Ramirez <franceslou_1818@yahoo.com>
2018-09-19 17:13:58 +0100

    finished lesson 3 of udacity VR interaction

---
+++ VR-Software-Development_VR-Interaction-2.0.0/VR Interaction/Assets/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+//	public void GotoScene() {
+//		//print ("test print");
+//		Debug.Log ("My method was called!");
+//		SceneManager.LoadScene ("00-FallingCoconut");
+//	}
+
+	// or use this instead of above
+	public void GotoScene(string sceneName) {
+		//print ("test print");
+		Debug.Log ("My method was called!");
+		SceneManager.LoadScene (sceneName);
+	}
+}
